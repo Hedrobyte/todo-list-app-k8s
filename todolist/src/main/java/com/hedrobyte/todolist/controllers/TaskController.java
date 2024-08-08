a4992038445849645a0d30d8f7ec9a55d3029928
hedrobyte <pedrohrmalves@gmail.com>
2024-08-08 16:51:01 -0300

    feat(controller): add controllers for handling requests

---
+++ todolist/src/main/java/com/hedrobyte/todolist/controllers/TaskController.java
@@ -0,0 +1,44 @@
+package com.hedrobyte.todolist.controllers;
+
+import com.hedrobyte.todolist.dtos.TaskDTO;
+import com.hedrobyte.todolist.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("tasks")
+public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<TaskDTO> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskDTO> getTaskById(@PathVariable Long id) {
+        TaskDTO taskDTO = taskService.getTaskById(id);
+        return taskDTO != null ? ResponseEntity.ok(taskDTO) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public TaskDTO createTask(@RequestBody TaskDTO taskDTO) {
+        return taskService.createTask(taskDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskDTO> updateTask(@PathVariable Long id, @RequestBody TaskDTO taskDTO) {
+        TaskDTO updatedTask = taskService.updateTask(id, taskDTO);
+        return updatedTask != null ? ResponseEntity.ok(updatedTask) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+}
