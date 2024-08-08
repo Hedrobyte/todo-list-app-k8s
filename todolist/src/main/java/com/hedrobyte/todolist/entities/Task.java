9038abd98557afd677c42513cdd7fe470f841313
hedrobyte <pedrohrmalves@gmail.com>
2024-08-08 16:52:15 -0300

    feat(entity): add entities for representing database tables

---
+++ todolist/src/main/java/com/hedrobyte/todolist/entities/Task.java
@@ -0,0 +1,21 @@
+package com.hedrobyte.todolist.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+}
+
