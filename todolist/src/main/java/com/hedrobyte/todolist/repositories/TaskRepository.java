7e65138b8eda2d7f8473b843662dbb8db47e719b
hedrobyte <pedrohrmalves@gmail.com>
2024-08-08 16:52:38 -0300

    feat(repository): add repositories for database access

---
+++ todolist/src/main/java/com/hedrobyte/todolist/repositories/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.hedrobyte.todolist.repositories;
+
+import com.hedrobyte.todolist.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
