4b565fc525180aca53fe0a6ef8b4234681b04187
hedrobyte <pedrohrmalves@gmail.com>
2024-08-17 15:45:36 -0300

    remove(frontend): delete unused directory

--- todolist/src/main/java/com/hedrobyte/todolist/repositories/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hedrobyte.todolist.repositories;
-
-import com.hedrobyte.todolist.entities.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
